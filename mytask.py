5ab80bc35627dea3d4ba23f6e42a29df5e9088b6
jiguangxia <xia95@126.com>
2017-01-02 14:24:40 +0800

    Create mytask.py

---
+++ mytask.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+#!usr/bin/python
+
+import time
+
+
+f = open('result.txt','w')
+for i in range(10):
+    f.write(str(i) + '\n')
+    time.sleep(1)
+
+f.close()
+
+
+open('__success__','w').close()
+
